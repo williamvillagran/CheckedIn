9685258913f81ba53b897d9fe9ffb03b8974ed40
vic1702 <danielgarc2002@gmail.com>
2025-05-05 13:16:33 -0500

    register activity created

--- app/src/main/java/edu/utsa/checkedin/HomeActivity.java
+++ app/src/main/java/edu/utsa/checkedin/HomeActivity.java
@@ -1,14 +1,29 @@
 package edu.utsa.checkedin;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button beginTripButton;
+    private Button findFriendsButton;
+    private Button addFriendsButton;
+    private Button settingsButton;
+
+
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        beginTripButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CollectionActivity.this, MainActivity.class);
+            startActivity(intent);
+            }
+        });
 
     }
 }
