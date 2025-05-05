561189b9734fc794bbc388e381d14fcdc244cc65
vic1702 <danielgarc2002@gmail.com>
2025-05-05 12:33:38 -0500

    changes to manifest and activities

--- app/src/main/java/edu/utsa/checkedin/HomeActivity.java
+++ app/src/main/java/edu/utsa/checkedin/HomeActivity.java
@@ -1,4 +1,14 @@
 package edu.utsa.checkedin;
 
-public class HomeActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
 }
--- app/src/main/java/edu/utsa/checkedin/MainActivity.java
+++ app/src/main/java/edu/utsa/checkedin/MainActivity.java
@@ -1,4 +1,13 @@
 package edu.utsa.checkedin;
 
-public class MainActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
