55873615cfebeeaa1181a420046ab0a3c8667f7e
vic1702 <danielgarc2002@gmail.com>
2025-05-05 16:08:52 -0500

    created buttons in home activity

--- app/src/main/java/edu/utsa/checkedin/HomeActivity.java
+++ app/src/main/java/edu/utsa/checkedin/HomeActivity.java
@@ -1,19 +1,55 @@
 package edu.utsa.checkedin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
-
 public class HomeActivity extends AppCompatActivity {
+
+    private Button beginButton;
+    private Button findFriendsButton;
+    private Button addFriendsButton;
+    private Button settingsButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        beginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BeginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        findFriendsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FindFriendsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        addFriendsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddFriendsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
-
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
